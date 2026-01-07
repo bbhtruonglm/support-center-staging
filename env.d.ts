005df7d91011f417b1aa6937b158886ddb3b29e4
Le Gia Uy <leuy091104@gmail.com>
2026-01-07 16:48:51 +0700

    style: scss cho scollbar

--- env.d.ts
+++ env.d.ts
@@ -19,6 +19,26 @@ declare module '*.vue' {
   export default component
 }
 
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
+
 declare module 'vue3-toastify' {
   import { Plugin } from 'vue'
 
